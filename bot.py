b5ea1e0feca3493f6f76d7422cb49aacaccde900
dario <darioabadie@hotmail.com>
2019-10-21 21:04:37 -0300

    Script bot.py comentado

--- bot.py
+++ bot.py
@@ -1,58 +1,46 @@
+
+# Librerías
+
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHandler, Filters
 from telegram import KeyboardButton, ReplyKeyboardMarkup
-import plotly.graph_objects as go
-
-
-
-# pip install python-telegram-bot
 from cajeros_func import cajeros
 
-
-
-
-G_API = "AIzaSyB3LR-c3c3mEJc6xUfYLTdspFFFPJQOYC4"
-
-# visitar esta página:
-# https://developers.google.com/maps/documentation/maps-static/intro
-
-
-
-#ubicacion = [-34.5835311,-58.4299928]
-ubicacion = [0,0]
+# Token de Telegram
 
 TOKEN = '935739772:AAEbQdhuWxUjUH3NSZwrEi4A0EnQTnKidAA'
 
-
-
-
+# Función que responde al comando "Banelco" ingresado por el usuario
 def banelco(bot, update):
     
     global red
     
     red = "BANELCO"
+    bot.send_message(chat_id=update.message.chat_id,text= "¡Bienvenido! A continuación verás los próximos a tu ubicación.")
 
-    location_keyboard = KeyboardButton(text="Enviar ubicación",  request_location=True)           #creating location button object
+    location_keyboard = KeyboardButton(text="Enviar ubicación",  request_location=True) #Creación del boton para enviar ubicación
     custom_keyboard = [[ location_keyboard]] 
     reply_markup = ReplyKeyboardMarkup(custom_keyboard) 
     update.message.reply_text(
-                    "Por favor serías tan amable de compartir tu ubicación?", 
+                    "¿Por favor serías tan amable de compartir tu ubicación?", 
                     reply_markup=reply_markup)
  
-    
+
+# Función que responde al comando "Link" ingresado por el usuario    
 def link (bot, update):
     
     global red
     
     red  = "LINK"
-    
-    location_keyboard = KeyboardButton(text="Enviar ubicación",  request_location=True)           #creating location button object
+    bot.send_message(chat_id=update.message.chat_id,text= "¡Bienvenido! A continuación verás los próximos a tu ubicación.")
+
+    location_keyboard = KeyboardButton(text="Enviar ubicación",  request_location=True) #Creación del boton para enviar ubicación
     custom_keyboard = [[ location_keyboard]] 
     reply_markup = ReplyKeyboardMarkup(custom_keyboard) 
     update.message.reply_text(
-                    "Por favor serías tan amable de compartir tu ubicación?", 
+                    "¿Por favor serías tan amable de compartir tu ubicación?", 
                     reply_markup=reply_markup)
  
-    
+# Función que se ejecuta cuando el usuario comparte su ubicación    
 def location(bot, update):
     message = None
     if update.edited_message:
@@ -60,35 +48,32 @@ def location(bot, update):
     else:
         message = update.message
    
+    ubicacion = [0,0] # Ubicación del usuario (Latitud y longitud)
     ubicacion[0] =  message.location.latitude
     ubicacion[1] =  message.location.longitude
     
     bot.send_message(chat_id=update.message.chat_id,text= "Estos son los cajeros más cercanos: ")
-    res = cajeros(ubicacion,red)
-    #mapa()
+    res = cajeros(ubicacion,red) # Se ejecuta la función "cajeros()" que identifica los cajeros cercanos a la ubicación del usuario.
     
     
-    chat_id = update.message.chat_id
-    for ind in range(0,len(res["banco"])):
+    #chat_id = update.message.chat_id
+    for ind in range(0,len(res["banco"])): # Se muestran en pantalla los cajeros cercanos (Banco y dirección) 
         bot.send_message(chat_id=update.message.chat_id,text=res["banco"][ind] 
         + " - " + res["ubicacion"][ind]
     )
-    #bot.send_photo(chat_id=update.message.chat_id, photo=open('fig1.jpg', 'rb'))
     
-    mensaje = mapa(ubicacion,res)
-    print(mensaje)
-    bot.send_message(chat_id=update.message.chat_id,text= mensaje)
-    #print(current_pos)
+    mensaje = mapa(ubicacion,res) # Se ejecuta la función "mapa()" que genera un mapa con los cajeros usando la API de Google
+    bot.send_message(chat_id=update.message.chat_id,text= mensaje) # Como resultado se envía al usuario el link con el mapa generado.
     
 
 def main():
+    
+    # Funciones encargadas de recibir los comandos ingresados por el usuario y asignar una función a cada uno.
     updater = Updater(TOKEN)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('BANELCO',banelco))
     dp.add_handler(CommandHandler('LINK',link))
-    dp.add_handler(MessageHandler(Filters.location, location, edited_updates=True))    
-
-    
+    dp.add_handler(MessageHandler(Filters.location, location, edited_updates=True))      
     updater.start_polling()
     updater.idle()
     
